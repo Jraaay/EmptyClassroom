25f282f247cd36487e27fd33b6e50253c36bed42
zhuorui.xx <zhuorui.xx@bytedance.com>
2024-01-10 14:38:17 +0800

    feat: 增加课表的处理脚本

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module EmptyClassroom
 go 1.21.5
 
 require (
+	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.9.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/robfig/cron/v3 v3.0.1
@@ -15,7 +16,6 @@ require (
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-contrib/static v0.0.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
@@ -26,9 +26,15 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/xuri/efp v0.0.0-20230802181842-ad255f2331ca // indirect
+	github.com/xuri/excelize/v2 v2.8.0 // indirect
+	github.com/xuri/nfp v0.0.0-20230819163627-dc951e3ffe1a // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
---
+++ script/convert_classtable.go
@@ -0,0 +1,290 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Class struct {
+	Campus  string    `json:"campus"`
+	Seat    string    `json:"seat"`
+	Name    string    `json:"name"`
+	Classes [][][]int `json:"classes"`
+}
+
+type ClassTable struct {
+	ClassList []Class           `json:"class"`
+	TypeMap   map[string]string `json:"typeMap"`
+}
+
+type CampusNameReConfig []struct {
+	Src  string
+	Repl string
+}
+
+type CampusIgnoreCondig []struct {
+	Re string
+}
+
+type CampusConfig struct {
+	Name               string
+	CampusNameReConfig CampusNameReConfig
+	CampusIgnoreCondig CampusIgnoreCondig
+}
+
+func main() {
+	xtcConfig := CampusConfig{
+		Name: "西土城",
+		CampusNameReConfig: CampusNameReConfig{
+			{
+				Src:  `主-(.+)`,
+				Repl: `主楼-$1`,
+			},
+			{
+				Src:  `(\d)-(.+)`,
+				Repl: `教$1-$2`,
+			},
+			{
+				Src:  `动画室`,
+				Repl: `教2-动画室`,
+			},
+			{
+				Src:  `新科研-(.+)`,
+				Repl: `新科研楼-$1`,
+			},
+			{
+				Src:  `明-(.+)`,
+				Repl: `明光楼-$1`,
+			},
+			{
+				Src:  `明光楼(\d.+)`,
+				Repl: `明光楼-$1`,
+			},
+			{
+				Src:  `经管楼(\d.+)`,
+				Repl: `经管楼-$1`,
+			},
+			{
+				Src:  `(东.)-(\d+)`,
+				Repl: `本部图书馆-$1$2`,
+			},
+			{
+				Src:  `图书馆一层`,
+				Repl: `本部图书馆-一层`,
+			},
+			{
+				Src:  `工程管理仿真中心`,
+				Repl: `学10-工程管理仿真中心`,
+			},
+		},
+		CampusIgnoreCondig: CampusIgnoreCondig{
+			{
+				Re: `网络教室`,
+			},
+			{
+				Re: `自行安排`,
+			},
+			{
+				Re: `虚拟教室`,
+			},
+			{
+				Re: `体育`,
+			},
+			{
+				Re: `工程管理仿真中心`,
+			},
+		},
+	}
+	shConfig := CampusConfig{
+		Name: "沙河",
+		CampusNameReConfig: CampusNameReConfig{
+			{
+				Src:  `（.*`,
+				Repl: ``,
+			},
+			{
+				Src:  `^([NS])(\d+)$`,
+				Repl: `$1-$2`,
+			},
+			{
+				Src:  `图-(.+)`,
+				Repl: `沙河图书馆-$1`,
+			},
+			{
+				Src:  `沙河图书馆东配楼`,
+				Repl: `沙河图书馆-东配楼_`,
+			},
+			{
+				Src:  `D-([NS])(.*)`,
+				Repl: `教学实验综合楼-D_$1$2`,
+			},
+			{
+				Src:  `邮政楼D1-(.*)`,
+				Repl: `邮政楼-D1_$1`,
+			},
+			{
+				Src:  `^D1-(.*)$`,
+				Repl: `邮政楼-D1_$1`,
+			},
+			{
+				Src:  `办-(.*)`,
+				Repl: `综合办公楼-$1`,
+			},
+			{
+				Src:  `地下一层自动化`,
+				Repl: `教学实验综合楼-地下一层自动化`,
+			},
+			{
+				Src:  `报告厅`,
+				Repl: `教学实验综合楼-报告厅`,
+			},
+			{
+				Src:  `学-(.*)`,
+				Repl: `学生活动中心-$1`,
+			},
+			{
+				Src:  `(..楼)(\d+)`,
+				Repl: `$1-$2`,
+			},
+			{
+				Src:  `电路中心(\d+)`,
+				Repl: `电路中心实验楼-$1`,
+			},
+		},
+		CampusIgnoreCondig: CampusIgnoreCondig{
+			{
+				Re: `网络教室`,
+			},
+			{
+				Re: `自行安排`,
+			},
+			{
+				Re: `虚拟教室`,
+			},
+			{
+				Re: `体育`,
+			},
+			{
+				Re: `旧`,
+			},
+		},
+	}
+	hnConfig := CampusConfig{
+		Name: "海南",
+		CampusNameReConfig: CampusNameReConfig{
+			{
+				Src:  `(..楼)(\d+)室?`,
+				Repl: `$1-$2`,
+			},
+		},
+		CampusIgnoreCondig: CampusIgnoreCondig{
+			{
+				Re: `网络教室`,
+			},
+			{
+				Re: `体育`,
+			},
+		},
+	}
+	convert(xtcConfig)
+	convert(shConfig)
+	convert(hnConfig)
+}
+
+func convert(campusConfig CampusConfig) {
+	f, err := excelize.OpenFile("script/raw/" + campusConfig.Name + ".xlsx")
+	if err != nil {
+		println(err.Error())
+		return
+	}
+	// 获取工作表中指定单元格的值
+	cell := f.GetSheetList()
+	rows, err := f.GetRows(cell[0])
+	classTable := ClassTable{
+		ClassList: []Class{},
+		TypeMap:   make(map[string]string),
+	}
+	classInfoRe, _ := regexp.Compile(`.*\s教室.\s?([^\s]+)\s*所属功能区.\s?([^\s(（]+)[(（](.*)[)）].*所属教学楼.\s?([^\s]+).*座位数.\s?([^\s]+)`)
+	classWeekRe, _ := regexp.Compile(`([^\s]+)周\[`)
+	for i := 0; i < len(rows); i++ {
+		class := Class{}
+		i++
+		classInfo := classInfoRe.FindAllStringSubmatch(rows[i][1], -1)
+		if len(classInfo) == 0 {
+			fmt.Println(rows[i][1])
+		}
+		class.Campus = classInfo[0][3]
+		class.Name = classInfo[0][1]
+		isIgnore := false
+		for _, reConfig := range campusConfig.CampusIgnoreCondig {
+			ignoreRe, _ := regexp.Compile(reConfig.Re)
+			if ignoreRe.MatchString(class.Name) {
+				isIgnore = true
+				break
+			}
+		}
+		if isIgnore {
+			i += 16
+			continue
+		}
+		for _, reConfig := range campusConfig.CampusNameReConfig {
+			nameRe, _ := regexp.Compile(reConfig.Src)
+			if nameRe.MatchString(class.Name) {
+				class.Name = nameRe.ReplaceAllString(class.Name, reConfig.Repl)
+			}
+		}
+		class.Seat = classInfo[0][5]
+		classTable.TypeMap[class.Name] = classInfo[0][2]
+		i++
+		i++
+		classes := [][][]int{}
+		for j := i; j < i+14; j++ {
+			var nowClass [][]int
+			for weekday := 1; weekday <= 7; weekday++ {
+				if weekday >= len(rows[j]) || rows[j][weekday] == "" {
+					nowClass = append(nowClass, []int{})
+					continue
+				}
+				classWeeksRaw := classWeekRe.FindAllStringSubmatch(rows[j][weekday], -1)
+				classWeeks := []int{}
+				for _, classWeek := range classWeeksRaw {
+					fromToList := strings.Split(classWeek[1], ",")
+					for _, fromToRaw := range fromToList {
+						fromTo := strings.Split(fromToRaw, "-")
+						if len(fromTo) == 1 {
+							week, err := strconv.ParseInt(fromTo[0], 10, 32)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							classWeeks = append(classWeeks, int(week))
+						} else {
+							from, err := strconv.ParseInt(fromTo[0], 10, 32)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							to, err := strconv.ParseInt(fromTo[1], 10, 32)
+							if err != nil {
+								fmt.Println(err.Error())
+							}
+							for week := from; week <= to; week++ {
+								classWeeks = append(classWeeks, int(week))
+							}
+						}
+					}
+				}
+				nowClass = append(nowClass, classWeeks)
+			}
+			classes = append(classes, nowClass)
+		}
+		class.Classes = classes
+		i += 14
+		classTable.ClassList = append(classTable.ClassList, class)
+	}
+	jsonByte, _ := json.Marshal(classTable)
+	os.WriteFile("script/classtable_config/"+campusConfig.Name+".json", jsonByte, os.ModePerm)
+}
